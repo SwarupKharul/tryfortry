a6200ab882beef4d89208a84466e885cb80c8683
Swarup Kharul <swarupkharulsk@gmail.com>
2021-03-20 13:28:12 +0530

    sign up

--- home/static/frontend/assets/js/script.js
+++ home/static/frontend/assets/js/script.js
@@ -14,35 +14,17 @@ var notesList = $('ul#notes');
 
 var noteContent = '';
 
-// Get all notes from previous sessions and display them.
 var notes = getAllNotes();
 renderNotes(notes);
 
-
-
-/*-----------------------------
-      Voice Recognition 
-------------------------------*/
-
-// If false, the recording will stop after a few seconds of silence.
-// When true, the silence period is longer (about 15 seconds),
-// allowing us to keep recording even when the user pauses. 
 recognition.continuous = true;
 
-// This block is called every time the Speech APi captures a line. 
-recognition.onresult = function(event) {
+recognition.onresult = function (event) {
 
-    // event is a SpeechRecognitionEvent object.
-    // It holds all the lines we have captured so far. 
-    // We only need the current one.
     var current = event.resultIndex;
 
-    // Get a transcript of what was said.
     var transcript = event.results[current][0].transcript;
 
-    // Add the current transcript to the contents of our Note.
-    // There is a weird bug on mobile, where everything is repeated twice.
-    // There is no official solution so far so we have to handle an edge case.
     var mobileRepeatBug = (current == 1 && transcript == event.results[0][0].transcript);
 
     if (!mobileRepeatBug) {
@@ -51,27 +33,22 @@ recognition.onresult = function(event) {
     }
 };
 
-recognition.onstart = function() {
+recognition.onstart = function () {
     instructions.text('Voice recognition activated. Try speaking into the microphone.');
 }
 
-recognition.onspeechend = function() {
+recognition.onspeechend = function () {
     instructions.text('You were quiet for a while so voice recognition turned itself off.');
 }
 
-recognition.onerror = function(event) {
+recognition.onerror = function (event) {
     if (event.error == 'no-speech') {
         instructions.text('No speech was detected. Try again.');
     };
 }
 
 
-
-/*-----------------------------
-      App buttons and input 
-------------------------------*/
-
-$('#start-record-btn').on('click', function(e) {
+$('#start-record-btn').on('click', function (e) {
     if (noteContent.length) {
         noteContent += ' ';
     }
@@ -79,24 +56,22 @@ $('#start-record-btn').on('click', function(e) {
 });
 
 
-$('#pause-record-btn').on('click', function(e) {
+$('#pause-record-btn').on('click', function (e) {
     recognition.stop();
     instructions.text('Voice recognition paused.');
 });
 
-// Sync the text inside the text area with the noteContent variable.
-noteTextarea.on('input', function() {
+noteTextarea.on('input', function () {
     noteContent = $(this).val();
 })
 
-$('#save-note-btn').on('click', function(e) {
+$('#save-note-btn').on('click', function (e) {
     recognition.stop();
 
     if (!noteContent.length) {
         instructions.text('Could not save empty note. Please add a message to your note.');
     } else {
-        // Save note to localStorage.
-        // The key is the dateTime with seconds, the value is the content of the note.
+
         saveNote(new Date().toLocaleString(), noteContent);
 
         // Reset variables and update UI.
@@ -109,7 +84,7 @@ $('#save-note-btn').on('click', function(e) {
 })
 
 
-notesList.on('click', function(e) {
+notesList.on('click', function (e) {
     e.preventDefault();
     var target = $(e.target);
 
@@ -127,11 +102,6 @@ notesList.on('click', function(e) {
     }
 });
 
-
-
-/*-----------------------------
-      Speech Synthesis 
-------------------------------*/
 
 function readOutLoud(message) {
     var speech = new SpeechSynthesisUtterance();
@@ -154,7 +124,7 @@ function readOutLoud(message) {
 function renderNotes(notes) {
     var html = '';
     if (notes.length) {
-        notes.forEach(function(note) {
+        notes.forEach(function (note) {
             html += `<li class="note">
         <p class="header">
           <span class="date">${note.date}</span>
